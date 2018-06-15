9d6a82d147930af2876cd815015a2e95387ddbe7
SHL-GROUP\Christian.Keller <christian.keller@shl-group.com>
2018-06-15 13:34:26 +0800

    Add node-persist with simple test

--- api/server.js
+++ api/server.js
@@ -1,12 +1,47 @@
 'use strict';
 
 const Hapi = require('hapi');
-const Mongojs = require('mongojs');
+const storage = require('node-persist');
 const Pack = require('./package');
 const Joi = require('joi');
 const Inert = require('inert');
 const Vision = require('vision');
 const HapiSwagger = require('hapi-swagger');
+
+
+//initiate the persistant storage
+(async () => {
+    
+    await storage.init({
+        dir: './data',
+        stringify: JSON.stringify,
+        parse: JSON.parse,
+        encoding: 'utf8',
+        logging: false,
+        ttl: false,
+        expiredInterval: 2*60*1000, // in milliseconds; every 2 minutes the process will clean-up the expired cache
+        forgiveParseErrors: false
+    }, function (err, content) {
+        if (err) {
+            return console.error(err)
+        }
+        console.log(content)
+    });
+    
+    let fibonacci = await storage.getItem('fibonacci');
+
+    if(! fibonacci) {
+        await storage.setItem('fibonacci', [0,1,1,2,3,5,8])
+            .then(() => {
+                console.log('Stored successfully');
+            })
+            .catch(err => console.error(err));
+        
+    }
+
+    fibonacci = await storage.getItem('fibonacci');
+    console.log('Fibonacci is ' + fibonacci); 
+})();
 
 
 
@@ -15,35 +50,27 @@ const server = Hapi.server({
     host: 'localhost'
 });
 
-//Connect to db
-var db = Mongojs('booked-beaver', ['resources']);
-
-db.on('error', function (err) {
-    console.log('database error', err)
-})
- 
-db.on('connect', function () {
-    console.log('database connected')
-
-    server.app.db = db;
-})
-
+// some test routes
 server.route({
     path: '/foobar/test',
     method: 'GET',
+    handler () {
+        return {};
+      },
     options: {
       tags: ['api'],
       description: 'My route description',
       notes: 'My route notes',
-      handler () {
-        return {};
-      }
+      
     }
 });
    
 server.route({
     path: '/foobar/{foo}/{bar}',
     method: 'GET',
+    handler () {
+        return {};
+    }, 
     options: {
       tags: ['api'],
       validate: {
@@ -51,9 +78,6 @@ server.route({
           foo: Joi.string().required().description('test'),
           bar: Joi.string().required()
         }
-      },
-      handler () {
-        return {};
       }
     }
 });
